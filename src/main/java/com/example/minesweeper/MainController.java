5111855cc6667919fcf0f39e5da555bbbd4b7ceb
Gusts Linkevics <gusts@gst.lv>
2021-03-04 21:38:23 +0200

    Game working

--- src/main/java/com/example/minesweeper/MainController.java
+++ src/main/java/com/example/minesweeper/MainController.java
@@ -1,21 +1,37 @@
 package com.example.minesweeper;
 
+import com.example.minesweeper.data.Board;
 import com.example.minesweeper.data.Game;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
     @GetMapping("")
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model)
+    {
+        if(MinesweeperApplication.game == null) {
+            MinesweeperApplication.game = new Game(5);
+        }
 
-        MinesweeperApplication.testValue += 1;
+        model.addAttribute("game", getGameHtml(MinesweeperApplication.game));
 
-        var game = new Game(5);
+        return "index";
+    }
 
-        model.addAttribute("game", getGameHtml(game));
-        model.addAttribute("counter", MinesweeperApplication.testValue);
+    @GetMapping("/check")
+    public String checkField(Model model, @RequestParam int row, @RequestParam int column) {
+
+        if(MinesweeperApplication.game == null) {
+            MinesweeperApplication.game = new Game(5);
+        }
+
+        MinesweeperApplication.game.checkField(row, column);
+
+        model.addAttribute("game", getGameHtml(MinesweeperApplication.game));
 
         return "index";
     }
@@ -26,11 +42,31 @@ public class MainController {
         for (int i = 0; i < game.getSize(); i++) {
             html.append("<div class=\"row\">");
             for (int j = 0; j < game.getSize(); j++) {
-                html.append("<div class=\"field\">" + game.getFieldValue(i, j) +"</div>");
+                html.append("<div class=\"field\">" + getFieldIcon(game.getFieldValue(i, j), i, j) +"</div>");
             }
             html.append("</div>");
         }
 
         return html.toString();
+    }
+
+    private String getFieldIcon(int i, int row, int column) {
+
+        switch (i) {
+            case Board.MINE_VALUE: {
+                return "<div class=\"field-bomb\"><img src=\"/img/bomb.svg\" class=\"field-icon\"></div>";
+             }
+             case Board.FIELD_FLAGGED: {
+                 return "<img src=\"/img/flag.svg\" class=\"field-icon\">";
+             }
+             case Board.FIELD_IS_EMPTY: {
+                 return "<div class=\"field-empty\">&nbsp;</div>";
+             }
+             case Board.FIELD_CLOSED: {
+                 return "<a class=\"field-closed\" href=\"/check?row="+row+"&column="+column+"\">&nbsp;</a>";
+             }
+        }
+
+        return "<div class=\"field-number\"><span class=\"number\">"+i+"</span></div>";
     }
 }
--- src/main/java/com/example/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/example/minesweeper/MinesweeperApplication.java
@@ -1,12 +1,13 @@
 package com.example.minesweeper;
 
+import com.example.minesweeper.data.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinesweeperApplication {
 
-	public static int testValue;
+	public static Game game;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MinesweeperApplication.class, args);
